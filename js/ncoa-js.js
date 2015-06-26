177c950b29cb8fe242542481c7d0312e2f5e5b0a
aaronlawrence34 <aaronlawrence@Swordfish.mailposte.ca>
2015-06-26 10:06:05 -0400

    page4 and 5
    page4 and 5

--- js/ncoa-js.js
+++ js/ncoa-js.js
@@ -4,10 +4,18 @@ $(document).ready(function(){
 		var percentOfStepWidth = 100/(numOfSteps-1);
 		var widthOfFirstAndLastStep = percentOfStepWidth*.5;
 		var widthOfMiddleStep = percentOfStepWidth;
-		var whichStep = $("#steps-breadcrumb ul li").index($('.step-current:eq(0)'));
-		var percentageBarWidth = (whichStep/(numOfSteps-1))*100;
+		var whichStep, percentageBarWidth;
+		
 		$("#steps-breadcrumb ul li:not(:first-child, :last-child)").css('width', widthOfMiddleStep+'%');
 		$("#steps-breadcrumb ul li:first-child, #steps-breadcrumb ul li:last-of-type").css('width', widthOfFirstAndLastStep+'%');
+		
+		if($("#steps-breadcrumb ul li:last-of-type").hasClass('step-done')){
+			percentageBarWidth = 100;
+		}else{
+			whichStep = $("#steps-breadcrumb ul li").index($('.step-current:eq(0)'));
+			percentageBarWidth = (whichStep/(numOfSteps-1))*100;
+		}
+		
 		$("#steps-breadcrumb div.progress-bar").css('width', percentageBarWidth+'%');
 	}
 });
