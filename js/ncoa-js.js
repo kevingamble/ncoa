6bbd583991df08eff2cdba043d2569c9a5336089
aaronlawrence34 <aaronlawrence@Swordfish.mailposte.ca>
2015-07-02 13:53:42 -0400

    update
    update

--- js/ncoa-js.js
+++ js/ncoa-js.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
 		var percentOfStepWidth = 100/(numOfSteps-1);
 		var widthOfFirstAndLastStep = percentOfStepWidth*.5;
 		var widthOfMiddleStep = percentOfStepWidth;
-		var whichStep, percentageBarWidth;
+		var whichStep, percentageBarWidth, currenttxt;
+		
+		if($("html").hasClass("ie8")){
+			$("#steps-breadcrumb ul li:first-child").addClass('first');
+			$("#steps-breadcrumb ul li:last-child").addClass('last');
+		}
 		
 		$("#steps-breadcrumb ul li:not(:first-child, :last-child)").css('width', widthOfMiddleStep+'%');
 		$("#steps-breadcrumb ul li:first-child, #steps-breadcrumb ul li:last-of-type").css('width', widthOfFirstAndLastStep+'%');
@@ -17,5 +22,17 @@ $(document).ready(function(){
 		}
 		
 		$("#steps-breadcrumb div.progress-bar").css('width', percentageBarWidth+'%');
+		
+		
+		if($("#steps-breadcrumb ul li").hasClass('step-current')){
+			currenttxt = $("#steps-breadcrumb ul li.step-current p").html();
+		}else if($("#steps-breadcrumb ul li").hasClass('step-done')){
+			currenttxt = $("#steps-breadcrumb ul li:last.step-done p").html();
+		}else{
+			currenttxt = $("#steps-breadcrumb ul li:first p").html();
+		}
+		$("#steps-breadcrumb div.stepname").html(currenttxt);
+		
+		
 	}
 });
